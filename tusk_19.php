229e0f0f8ed10c20632439ed0c45aaf022303fe3
Roshaa2023 <hesenovresad362@gmail.com>
2025-08-24 21:26:59 +0400

    yeniliklər əlavə olundu

--- tusk_19.php
+++ tusk_19.php
@@ -1,18 +1,54 @@
-<!doctype html>
-<html lang="en">
+<?php
+session_start();
+
+$db_username = "Rashad";
+$db_password = "12345";
+
+if (isset($_POST['username'], $_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $db_username && $password === $db_password) {
+        $_SESSION['username'] = $username;
+
+        setcookie("last_login", date("Y-m-d H:i:s"), time() + (86400 * 365), "/");
+
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "İstifadəçi adı və ya şifrə səhvdir!";
+    }
+}
+echo '<hr><br>';
+?>
+
+<!DOCTYPE html>
+<html lang="az">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>tusk_19</title>
+    <title>example_20/Login</title>
 </head>
 <body>
-<center><h3><i>Session istifadə edərək istifadəçi daxil olub olmadığını yoxlayın. Daxil olubsa "Xoş gəlmisiniz!", yoxsa "Zəhmət olmasa daxil olun" mesajını verin</i></h3></center>
-<center style="margin: 10%">
+<center>
+    <center><h1><pre>sadə Login sistemi yaradırıq Session və Cookie - dən istifadə edirik</pre></h1></center>
+    <?php if (isset($_SESSION['username'])): ?>
+        <h2>Xoş gəlmisiniz, <?= $_SESSION['username'] ?>!</h2>
 
+        <?php if (isset($_COOKIE['last_login'])): ?>
+            <p>Son giriş tarixi: <?= $_COOKIE['last_login'] ?></p>
+        <?php endif; ?>
+
+    <?php else: ?>
+    <h2>Daxil olun</h2>
+    <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <div class="container">
+        <form action="" method="POST">
+            İstifadəçi adı: <input type="text" name="username" required>
+            Şifrə: <input type="password" name="password" required>
+            <input type="submit" value="Daxil ol">
+        </form>
+        <?php endif; ?></div>
 
 </center>
 </body>
 </html>
-
